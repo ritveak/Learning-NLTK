05cad3af8095263fbe0d935e96ba7950fd48b9d9
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2018-12-05 19:42:47 +0530

    Delete tokenize word and sentence.py

--- tokenize word and sentence.py
+++
@@ -1,8 +0,0 @@
-from nltk.tokenize import sent_tokenize,word_tokenize
-
-example_text = "Hello, Mr. Ritveak, How are you doing? I hope everything is fine. Don't fall asleep every time! Bye for now."
-for i in sent_tokenize(example_text):
-    print(i)
-
-for i in word_tokenize(example_text):
-    print(i)
